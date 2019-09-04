f706f3dd62b16e811cfcb5c15aedade31053379c
yasmin <yasminefawzy22@gmail.com>
2019-09-04 15:00:20 +0200

    Updates

---
+++ precache-manifest.4a66138da25f62351a845a93164bbb10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475528c5d92fef0ad78f060947f1563d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b053413f5fcae0d55347",
+    "url": "./static/css/main.7accacef.chunk.css"
+  },
+  {
+    "revision": "61a13daabf1050315e3f",
+    "url": "./static/js/2.61faad0b.chunk.js"
+  },
+  {
+    "revision": "b053413f5fcae0d55347",
+    "url": "./static/js/main.c58620c2.chunk.js"
+  },
+  {
+    "revision": "0e195334ad06fbc6399b",
+    "url": "./static/js/runtime~main.67af940d.js"
+  }
+]);
